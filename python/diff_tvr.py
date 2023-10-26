fix data-data[0] with data[1:]-data[0], add x axis for derivative figure, and other minor adjustments

python/diff_tvr.py
@@ -114,8 +114,7 @@ class DiffTVR:
             np.array: Vector of length N+1
         """
 
-        return (self.a_mat_t.matvec(self.a_mat.matvec(deriv_curr)) -
-                self.a_mat_t.matvec(data - data[0]) + ln_mat @ deriv_curr)
+        return self.a_mat_t.matvec(self.a_mat.matvec(deriv_curr)) - self.a_mat_t.matvec(data) + ln_mat @ deriv_curr
 
     def make_hn_mat(self, ln_mat: np.array) -> LinearOperator:
         """Matrix in linear problem
@@ -166,7 +165,7 @@ class DiffTVR:
         """Get derivative via TVR over optimization steps
 
         Args:
-            data (np.array): Data of size N
+            data (np.array): Data of size N+1
             deriv_guess (np.array): Guess for derivative of size N+1
             alpha (float): Regularization parameter
             no_opt_steps (int): No. opt steps to run
@@ -177,11 +176,11 @@ class DiffTVR:
             Tuple[np.array,np.array]: First is the final derivative of size N+1,
             second is the stored derivatives if return_progress=True of size no_opt_steps+1 x N+1, else [].
         """
-
+        data = data[1:] - data[0]
         deriv_curr = deriv_guess
         deriv_st = []
         for opt_step in range(no_opt_steps):
-            update = self.get_deriv_tvr_update(data=data, deriv_curr=deriv_curr, alpha=alpha)
+            update = self.get_deriv_tvr_update(data, deriv_curr, alpha)
             deriv_curr += update
             if return_progress:
                 if opt_step % return_interval == 0:

python/example_abs.py
@@ -8,13 +8,18 @@ if __name__ == "__main__":
     start = time.time()
 
     # Data
+    def func(x):
+        return abs(x - 0.5)
+
+    def deriv_func(x):
+        return np.where(x < 0.5, -1, 1)
+
     n = 1000
-    end = 1
-    x_coords = np.linspace(0, end, n+1)
-    data = abs(x_coords - 0.5)
+    x_coords = np.linspace(0, 1, n+1)
+    data = func(x_coords)
 
     # True derivative
-    deriv_true = np.where(x_coords < 0.5, -1, 1)
+    deriv_true = deriv_func(x_coords)
 
     # Add noise
     data_noisy = data + np.random.normal(0, 0.05, n+1)
@@ -30,7 +35,7 @@ if __name__ == "__main__":
     dx = np.diff(x_coords)
     diff_tvr = DiffTVR(n, dx, maxiter=None)
     deriv, progress = diff_tvr.get_deriv_tvr(
-        data=data_noisy[:-1],
+        data=data_noisy,
         deriv_guess=np.ones(n + 1),
         alpha=0.2,
         no_opt_steps=100,
@@ -38,8 +43,8 @@ if __name__ == "__main__":
 
     # Plot TVR derivative
     fig2 = plt.figure()
-    plt.plot(deriv_true)
-    plt.plot(deriv)
+    plt.plot(x_coords, deriv_true)
+    plt.plot(x_coords, deriv)
     plt.title("Derivative")
     plt.legend(["True", "TVR"])
 